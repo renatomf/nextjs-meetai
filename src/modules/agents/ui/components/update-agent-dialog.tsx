82cbda71b8937f25d65ac83f161bd09c040688c1
renatomarf76 <renatomarf76@egmail.com>
2025-06-14 16:51:52 -0300

    15-Agent Update  & Delete

--- src/modules/agents/ui/components/update-agent-dialog.tsx
+++ src/modules/agents/ui/components/update-agent-dialog.tsx
@@ -1,27 +1,31 @@
 import { ResponsiveDialog } from "@/components/responsive-dialog";
 
 import { AgentForm } from "./agent-form";
+import { AgentGetOne } from "../../types";
 
 interface Props {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  initialValues: AgentGetOne;
 }
 
-export const NewAgentDialog = ({
+export const UpdateAgentDialog = ({
   open,
   onOpenChange,
+  initialValues,
 }: Props) => {
 
   return (
     <ResponsiveDialog
-      title="New Agent"
-      description="Create a new agent"
+      title="Edit Agent"
+      description="Edit the agent details"
       open={open}
       onOpenChange={onOpenChange}
     >
       <AgentForm
         onSuccess={() => onOpenChange(false)}
         onCancel={() => onOpenChange(false)}
+        initialValues={initialValues}
       />
     </ResponsiveDialog>
   );
